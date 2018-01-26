be6bcd891b1c1438bbf0addae7ad0663789a11ac
Lars <Lars.Kruik@live.nl>
2018-01-26 20:59:47 +0100

    Added first primitive dinosaur movement

--- GlobalGamejam2018/Assets/Scripts/DinoBase.cs
+++ GlobalGamejam2018/Assets/Scripts/DinoBase.cs
@@ -2,15 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DinoBase : MonoBehaviour {
+public class DinoBase : MonoBehaviour
+{
+    [Header("Base dino properties")]
+    public int health       = 1;
+    public float speed      = 10.0f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float changedirectionChance = 0.5f;
+    public Vector2 travelTime = new Vector2(0.5f, 3.0f);
+
+    private bool m_IsMovingRight = true;
+    private float m_TravelTimeCounter = 0.0f;
+
+    private void Start()
+    {
+        if (Random.value < 0.5f)
+            m_IsMovingRight = !m_IsMovingRight;
+    }
+
+    public void Init()
+    {
+        
+    }
+
+    private void Update()
+    {
+        Vector2 newPosition = Vector2.zero;
+
+        if (m_IsMovingRight)
+           newPosition = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y);
+        else if(!m_IsMovingRight)
+            newPosition = new Vector2(transform.position.x + speed * Time.deltaTime * -1.0f, transform.position.y);
+
+        transform.position = newPosition;
+    }
+
+
 }
